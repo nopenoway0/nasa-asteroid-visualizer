011c55640692b917144a8c56271162b6a4ab9bd6
nopenoway0 <benjamin.gottheil@sjsu.edu>
2018-12-11 00:30:43 -0800

    added scaled rotations

--- __init__.py
+++ __init__.py
@@ -11,9 +11,12 @@ if __name__ =='__main__':
     # a simple page that says hello
     @app.route('/')
     def get_visualizer():
+        planet_rotations = None
+        with open('static/planet_rotations.json', 'r') as f:
+            planet_rotations = json.load(f)
         texture_locations = {}
         for planet in ['Sun', 'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']:
-            texture_locations[planet] = url_for('static', filename=planet + '.jpg')
+            texture_locations[planet] = url_for('static', filename=planet + '.jpg'), planet_rotations[planet]
         print(url_for('static', filename='sun.jpg'))
         meteors = []
         with open('static/example_response.json', 'r') as f:
@@ -24,6 +27,6 @@ if __name__ =='__main__':
                     meteors.append(meteor.serialize())
                 except:
                     pass#print('error with meteor: ' + str(meteor))
-        return render_template('meteor_template.html', meteors=meteors, texture_locations=texture_locations)
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8000)))
+        return render_template('meteor_template.html', meteors=meteors, planet_info=texture_locations)
+    app.run()#host='0.0.0.0', port=int(os.getenv('PORT', 8000)))
 
