ff98a189844de9fe8160b7c929ad8332198a6ae7
nopenoway0 <benjamin.gottheil@sjsu.edu>
2018-12-10 21:44:57 -0800

    initial commit

--- __init__.py
+++ __init__.py
@@ -5,27 +5,9 @@ from flask import Flask, url_for, render_template, redirect
 from NASAObjects import NasaMeteor
 import json
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
+# start app
+if __name__ =='__main__':
+    app = Flask(__name__)
     # a simple page that says hello
     @app.route('/')
     def get_visualizer():
@@ -43,5 +25,5 @@ def create_app(test_config=None):
                 except:
                     pass#print('error with meteor: ' + str(meteor))
         return render_template('meteor_template.html', meteors=meteors, texture_locations=texture_locations)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8000)))
 
-    return app
